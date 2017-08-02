082ea88028f3693e56b3acbed52d89695d168890
Nirvana <nirvana@myster.io>
2017-08-02 01:45:47 -0400

    Updated language in script, added test cases

--- brain2BIP39.sh
+++ brain2BIP39.sh
@@ -1,7 +1,8 @@
-printf "This script turns a brainwallet phrase in to a BIP-39 compatible mnemonic for hardware wallets, etc.\n\n"
-printf "It is intended to be used with DiceWare brainwallets with at least 128bits of entropy. But it doesn't verify entropy.\n\n"
+printf "This script turns a brainwallet phrase in to a BIP-39 compatible mnemonic that can be used to restore your wallet.\n\n"
+printf "It is intended to be used with DiceWare brainwallets with at least 128bits of entropy. \n\n"
+printf "Because the BIP-39 wordlist is only 2k words and designed for three leter uniquenes, more entropy can be stored in fewer words with a Diceware phrase, using the nearlt 8k word list.\n\n"
 
 read -p "Enter Brain Wallet: " brainwallet
 
 mnemonic=`bx base16-encode "$brainwallet" | bx bitcoin256 | bx mnemonic-new`
-printf "BIP-39 mnemonic: (save wallet 24 words) \n\n$mnemonic\n\n"
+printf "BIP-39 mnemonic / Wallet Restoration Phrase:  \n\n$mnemonic\n\n"
